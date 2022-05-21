2ad3c1a575e933684f98d91d126c0d93197c18a6
Yunchoel Lee <yun@yunchoels-air.local>
2022-05-20 22:47:34 -0600

    clean up and set ui for header

--- clients/src/views/Header.tsx
+++ clients/src/views/Header.tsx
@@ -1,4 +1,7 @@
 import './Header.css'
+import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 interface Texting {
   text: string,
@@ -8,8 +11,13 @@ export default function Header () {
   return(
     <div className="header">
       <h3>Yunber Eats</h3>
-      <input type="text" placeholder="search restaurants" id="searchBar" />
-      <button>login</button>
+      <div className='head-body'>
+        <Button id='location' variant="contained" startIcon={<LocationOnIcon />} onClick= {() => {console.log("location")}}>
+          Location
+        </Button>
+        <TextField variant='filled' type="text" placeholder="search restaurants" id="searchBar"/>
+      </div>
+      <Button variant='contained' onClick={() => {console.log("login")}}>Login</Button>
     </div>
 
   )
