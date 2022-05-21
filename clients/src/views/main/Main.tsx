5fc5233619dc745890c6c2e975df6adaaef23541
Yunchoel Lee <yun@yunchoels-air.local>
2022-05-20 23:55:05 -0600

    building body

---
+++ clients/src/views/main/Main.tsx
@@ -0,0 +1,9 @@
+import './main.css'
+
+export default function Main () {
+  return (
+    <main className='images'>
+      <img src={require('/Users/yun/Desktop/work/ts/uber-eats/clients/src/images/first.jpg')} alt="" />
+    </main>
+  )
+}
