9cd5dd8f61ef47688afbe84a578ef51b17d44084
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2022-05-31 20:01:39 -0700

    random image for now

--- clients/src/views/Restaurants/list.tsx
+++ clients/src/views/Restaurants/list.tsx
@@ -20,11 +20,11 @@ export default function RestaurantsList () {
   }
 
   return (
-      <main style={{ width: '70rem', height: '600px', margin: '10px auto', overflow: 'hidden'}} id="Body">
+      <main style={{ width: '70rem', margin: '10px auto', overflow: 'hidden'}} id="Body">
         {tempList.map((item: string) => {
           return(
             <div className="restaurant" onClick={() => {console.log(item)}}>
-              <img src={images[random(images.length)]} alt="" style={{width: "10rem", height: "100px", margin: "auto 0", borderRadius: "20px"}}/>
+              <img src={images[random(images.length)]} alt="" style={{width: "10rem", height: "150px", margin: "auto 0", borderRadius: "20px"}}/>
               <p style={{marginLeft: "10px"}}>{item}</p>
             </div>
           )
